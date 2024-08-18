0cd6abfa41663fc11e63222f2520350a7e6b4415
JulianTocasuche <julistv1234@gmail.com>
2024-08-18 12:43:53 -0500

    Initial commit

---
+++ sentencia_if.py
@@ -0,0 +1,9 @@
+print(f'*** Sentencia if ***')
+
+edad = 12
+if edad >= 18:
+    print(f'Eres mayor de edad. Tienes {edad} años')
+elif 13 <= edad < 18:
+    print(f'Eres un adolescente, Tienes {edad} años')
+else:
+    print(f'Eres menor de edad. Tienes {edad} años')
